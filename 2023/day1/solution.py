617b7510cd33bed8b393426ccec1d1d8bf02caad
Jonathan Biemond <jbiemond@my.bcit.ca>
2023-12-01 06:25:10 -0800

    2023-day1: part 2 solution

--- 2023/day1/solution.py
+++ 2023/day1/solution.py
@@ -3,6 +3,7 @@
 with open('2023/day1/input.txt', 'r') as f:
     data = f.read()
 
+# Part 1
 cal_vals = []
 
 for line in data.splitlines():
@@ -10,3 +11,39 @@ for line in data.splitlines():
     cal_vals.append(int(digits[0] + digits[-1]))
 
 print("Part 1: ", sum(cal_vals))
+
+
+# Part 2
+def first_numeric(string: str, num_map: dict[str, int]) -> str:
+    string_start = ""
+    for char in string:
+        if char.isnumeric():
+            return char
+        string_start += char
+        for word, digit in num_map.items():
+            if word in string_start:
+                return str(digit)
+
+
+cal_vals = []
+numbers = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+numbers_r = {word[::-1]: digit for word, digit in numbers.items()}
+for line in data.splitlines():
+    cal_vals.append(
+        int(
+            first_numeric(line, numbers)
+            + first_numeric(line[::-1], numbers_r)
+        )
+    )
+
+print("Part 2: ", sum(cal_vals))
