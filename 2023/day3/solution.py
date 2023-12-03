d9be752da1615af86196f55bccd9a7e45f21e7e4
Jonathan Biemond <jbiemond@my.bcit.ca>
2023-12-02 22:20:41 -0800

    2023-day3: part 2 solution

--- 2023/day3/solution.py
+++ 2023/day3/solution.py
@@ -1,16 +1,19 @@
 """2023 Day 3: Gear Ratios"""
+import math
 import re
+from collections import defaultdict
 from itertools import islice
 
 with open('2023/day3/input.txt', 'r') as f:
     data = f.read()
 
-i = 0
+# Part 1
+n = 0
 line_len = len(data.split(maxsplit=1)[0])
 data = data.replace("\n", "")
-digits = re.finditer(r"\d+", data)
+numbers = re.finditer(r"\d+", data)
 pattern = re.compile(r"[^.\d]")
-for match in digits:
+for match in numbers:
     span = match.span()
     adjacent_chars = (
         data[span[0]-1]
@@ -31,6 +34,29 @@ for match in digits:
         )
     )
     if pattern.search(adjacent_chars):
-        i += int(match.group())
+        n += int(match.group())
 
-print("Part 1: ", i)
+print("Part 1: ", n)
+
+# Part 2
+n = 0
+
+gears = re.finditer(r"\*", data)
+numbers = re.finditer(r"\d+", data)
+indices = defaultdict(list[int])
+
+for match in numbers:
+    number = int(match.group())
+    span = match.span()
+    indices[span[0] - 1] += [number]
+    indices[span[1]] += [number]
+    for i in range(span[0] - 1, span[1] + 1):
+        indices[i + line_len] += [number]
+        indices[i - line_len] += [number]
+
+for gear in gears:
+    nums = indices.get(gear.span()[0], "")
+    if len(nums) == 2:
+        n += math.prod(nums)
+
+print("Part 2: ", n)
